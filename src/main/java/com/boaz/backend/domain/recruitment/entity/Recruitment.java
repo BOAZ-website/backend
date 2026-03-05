78b07bf81f16fb5de87cd8c4515c4b2e1485be9d
seoyeon83 <mool848777@naver.com>
2026-03-06 00:50:42 +0900

    #6 feat: term 컬럼 unique 특성 추가

--- src/main/java/com/boaz/backend/domain/recruitment/entity/Recruitment.java
+++ src/main/java/com/boaz/backend/domain/recruitment/entity/Recruitment.java
@@ -15,7 +15,7 @@ public class Recruitment extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private Integer term;
 
     @Column(nullable = false)
