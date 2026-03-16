56930707e6ac040cd803c773bde2d07fc962f5ab
jaewonnow <1219sjw@gmail.com>
2026-03-16 11:01:24 +0900

    feat: Curriculum 도메인 기능 추가

--- src/main/java/com/boaz/backend/domain/recruitment/repository/SubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boaz.backend.domain.recruitment.repository;
-
-import com.boaz.backend.domain.recruitment.entity.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-
-    boolean existsByEmail(String email);
-}
