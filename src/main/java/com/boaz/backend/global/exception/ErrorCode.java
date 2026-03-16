e273cb50faac14c8d016fe59629651046d75aef8
jaewonnow <1219sjw@gmail.com>
2026-03-16 10:20:42 +0900

    fix: ERRORCODE 수정

--- src/main/java/com/boaz/backend/global/exception/ErrorCode.java
+++ src/main/java/com/boaz/backend/global/exception/ErrorCode.java
@@ -16,7 +16,7 @@ public enum ErrorCode {
 
     // Curriculum
     CURRICULUM_NOT_FOUND(HttpStatus.NOT_FOUND, "CURRICULUM_NOT_FOUND", "해당 커리큘럼을 찾을 수 없습니다."),
-    INVALID_TRACK(HttpStatus.BAD_REQUEST, "INVALID_TRACK", "유효하지 않은 트랙입니다."),
+    
     // Review
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW_NOT_FOUND", "해당 후기를 찾을 수 없습니다."),
 
