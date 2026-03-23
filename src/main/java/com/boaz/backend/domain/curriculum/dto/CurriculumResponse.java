98df78337992b39cde70eee4bfc8b5fcac32e8f8
jaewonnow <1219sjw@gmail.com>
2026-03-23 22:07:36 +0900

    #43 docs : 예시 데이터 설명 추가

--- src/main/java/com/boaz/backend/domain/curriculum/dto/CurriculumResponse.java
+++ src/main/java/com/boaz/backend/domain/curriculum/dto/CurriculumResponse.java
@@ -2,6 +2,7 @@ package com.boaz.backend.domain.curriculum.dto;
 
 import com.boaz.backend.domain.curriculum.entity.Curriculum;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.util.List;
@@ -9,10 +10,14 @@ import java.util.List;
 @Getter
 public class CurriculumResponse {
 
+    @Schema(description = "커리큘럼 ID", example = "1")
     private final Long id;
+
+    @Schema(description = "트랙", example = "ANALYSIS", allowableValues = {"ANALYSIS", "VISUALIZATION", "ENGINEERING"})
     private final String track;
 
     @JsonProperty("curriculum_steps")
+    @Schema(description = "커리큘럼 단계 목록")
     private final List<CurriculumStepResponse> curriculumSteps;
 
     private CurriculumResponse(Long id, String track, List<CurriculumStepResponse> curriculumSteps) {
--- src/main/java/com/boaz/backend/domain/curriculum/dto/CurriculumStepResponse.java
+++ src/main/java/com/boaz/backend/domain/curriculum/dto/CurriculumStepResponse.java
@@ -1,12 +1,18 @@
 package com.boaz.backend.domain.curriculum.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class CurriculumStepResponse {
 
+    @Schema(description = "단계 번호", example = "1")
     private final int step;
+
+    @Schema(description = "단계 제목", example = "데이터 분석 기초")
     private final String title;
+
+    @Schema(description = "단계 설명", example = "통계 기초, 파이썬 기초")
     private final String desc;
 
     public CurriculumStepResponse(int step, String title, String desc) {
