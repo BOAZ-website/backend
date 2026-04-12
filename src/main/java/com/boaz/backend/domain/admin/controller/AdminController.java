f66905b4020d8bc976008ba955350f25585ef1ee
seoyeon83 <mool848777@naver.com>
2026-04-12 17:50:48 +0900

    #56 refactor: 지원서 파일 내려받기 기능 위치 변경 (AdminController -> RecruitmentAdminController)

--- src/main/java/com/boaz/backend/domain/admin/controller/AdminController.java
+++ src/main/java/com/boaz/backend/domain/admin/controller/AdminController.java
@@ -1,19 +1,8 @@
 package com.boaz.backend.domain.admin.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.boaz.backend.domain.recruitment.service.RecruitmentService;
-import com.boaz.backend.global.common.ApiResponse;
-import com.boaz.backend.global.exception.CustomException;
-import com.boaz.backend.global.exception.ErrorCode;
-
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -23,19 +12,4 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/admin")
 public class AdminController {
 
-    @Value("${admin.api-key}")
-    private String adminKeyValue;
-    private final RecruitmentService recruitmentService;
-
-    @Operation(summary = "지원서 CSV 파일 생성")
-    @PostMapping("/recruitment/applications/download")
-    public ResponseEntity<ApiResponse<Void>> downloadApplications(
-            @RequestParam Integer term,
-            @RequestHeader("X-Admin-Key") String adminKey) {
-        if (!adminKey.equals(adminKeyValue)) {
-            throw new CustomException(ErrorCode.UNAUTHORIZED);
-        }
-        recruitmentService.downloadApplications(term);
-        return ResponseEntity.ok(ApiResponse.ok(null));
-    }
 }
