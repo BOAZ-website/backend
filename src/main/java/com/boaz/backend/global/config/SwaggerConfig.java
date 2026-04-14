6a9f589dbc8204cb4956fd809d01b015b1f6faff
seoyeon83 <mool848777@naver.com>
2026-04-14 23:19:01 +0900

    #58 chore: swagger JWT Bearer 인증 설정 추가

--- src/main/java/com/boaz/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/boaz/backend/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.boaz.backend.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +17,8 @@ import io.swagger.v3.core.jackson.ModelResolver;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String BEARER_AUTH = "bearerAuth";
+
     @Value("${swagger.server-url}")
     private String serverUrl;
 
@@ -24,7 +29,14 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("BOAZ 웹사이트 백엔드 API")
                         .description("BOAZ 웹사이트 백엔드 API 문서")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+                                .name(BEARER_AUTH)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 
     @Bean
