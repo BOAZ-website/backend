950d2339c5228edc24edc7f2f713fb982d12d5e4
seoyeon83 <mool848777@naver.com>
2026-04-14 23:37:23 +0900

    #58 fix: Swagger 보안 설정 전역 적용 제거

--- src/main/java/com/boaz/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/boaz/backend/global/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.boaz.backend.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,7 +29,6 @@ public class SwaggerConfig {
                         .title("BOAZ 웹사이트 백엔드 API")
                         .description("BOAZ 웹사이트 백엔드 API 문서")
                         .version("v1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
                 .components(new Components()
                         .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
                                 .name(BEARER_AUTH)
