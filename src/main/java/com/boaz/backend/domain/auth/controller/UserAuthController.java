8e751a05e8c452126040a9adc685a454e4882dbd
seoyeon83 <mool848777@naver.com>
2026-05-18 23:42:24 +0900

    chore: Swagger @Tag 명세 컨벤션 통일 ([Admin]/[User] prefix)

--- src/main/java/com/boaz/backend/domain/auth/controller/UserAuthController.java
+++ src/main/java/com/boaz/backend/domain/auth/controller/UserAuthController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "User Auth", description = "User 인증 관련 API")
+@Tag(name = "[User] Auth", description = "User 인증 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth/user")
