0517adee1afe40dd97357642d92d11f9446c6dba
seoyeon83 <mool848777@naver.com>
2026-05-21 21:44:31 +0900

    fix: UserInfoResponse에서 member_type 제거

--- src/main/java/com/boaz/backend/domain/user/dto/response/UserInfoResponse.java
+++ src/main/java/com/boaz/backend/domain/user/dto/response/UserInfoResponse.java
@@ -1,7 +1,6 @@
 package com.boaz.backend.domain.user.dto.response;
 
 import com.boaz.backend.domain.user.entity.User;
-import com.boaz.backend.global.common.enums.MemberType;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,12 +9,10 @@ import lombok.Getter;
 public class UserInfoResponse {
 
     private String nickname;
-    private MemberType memberType;
 
     public static UserInfoResponse from(User user) {
         return UserInfoResponse.builder()
                 .nickname(user.getNickname())
-                .memberType(user.getMemberType())
                 .build();
     }
 }
